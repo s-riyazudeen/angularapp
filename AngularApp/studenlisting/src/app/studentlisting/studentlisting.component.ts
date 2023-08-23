c7b78db835eecd6bda9fc89182023e4559334dd0
Mohammed Riyazudeen <141828993+s-riyazudeen@users.noreply.github.com>
2023-08-23 15:21:45 +0530

    Add files via upload

---
+++ AngularApp/studenlisting/src/app/studentlisting/studentlisting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-studentlisting',
+  templateUrl: './studentlisting.component.html',
+  styleUrls: ['./studentlisting.component.css']
+})
+export class StudentlistingComponent {
+  
+
+}
